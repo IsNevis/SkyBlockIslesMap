8583e4479f8af130737a784f1990d4b32ea14212
IsItNevis <94104284+IsNevis@users.noreply.github.com>
2022-01-09 16:13:02 +0530

    Tried to Remove Spawn Marker

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -27,7 +27,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": false,
-        "cacheTag": "1641680519",
+        "cacheTag": "1641724942",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
